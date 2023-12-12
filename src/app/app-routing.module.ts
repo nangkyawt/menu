f8e5c07759f06137f576dbfe6ed163cfd6988526
nangkyawt <nangkyawthsuhlaing@gmail.com>
2023-12-12 09:53:48 +0630

    second commi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: HomeComponent,
   },
